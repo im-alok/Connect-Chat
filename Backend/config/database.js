05e39e47ce25ab3125df0b1b42e42a26b3bbff22
im-alok <alok.ranjan.aiml25@heritageit.edu.in>
2024-03-19 22:17:20 +0530

    connection to the database

---
+++ Backend/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+function dbConnect() {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log('DataBase connection successfully'))
+        .catch((error) => console.log('Something occur during Database connectivity'));
+}
+
+module.exports = dbConnect
