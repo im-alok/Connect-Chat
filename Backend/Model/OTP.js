0a9603d8569edf754f078c3868f94635d2a0c619
im-alok <alok.ranjan.aiml25@heritageit.edu.in>
2024-03-20 08:47:29 +0530

    sending mail to user using nodemailer

--- Backend/Model/OTP.js
+++ Backend/Model/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../config/mailSender');
 const otpSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -14,6 +15,21 @@ const otpSchema = new mongoose.Schema({
         default:Date.now(),
         expires:60 * 5
     }
+})
+
+async function sendVerificationMail(email,otp){
+    try {
+        const response = await mailSender(email,verification | ConnectChat , otp);
+        // console.log(response);
+    } catch (error) {
+        console.log(error);
+        console.log('Error from model in sending verification mail');
+    }
+}
+
+otpSchema.pre("save",async function(next){
+    await sendVerificationMail(this.email,this.otpValue);
+    next();
 })
 
 exports.module = mongoose.model('OTP',otpSchema);
